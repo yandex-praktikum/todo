aca22815df14f3a7f3a763f7c32f4f77d05cf425
Andy Pronin <proninc@yandex.ru>
2020-11-18 18:16:53 +0400

    add settings.MEDIA_ROOT, pep8

--- deals/tests/test_views.py
+++ deals/tests/test_views.py
@@ -1,16 +1,9 @@
-import os
-import shutil
-import tempfile
-from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 from django import forms
 
 from deals.models import Task
-
-
 
 
 class TaskPagesTests(TestCase):
